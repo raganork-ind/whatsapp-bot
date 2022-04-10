ac826d60434ef38b42c3b1b7a0841d432d58ba8b
Sourav <87891508+souravkl11@users.noreply.github.com>
2022-04-10 22:48:41 +0530

    Beta release

---
+++ config.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+DATABASE_URL = process.env.DATABASE_URL === undefined ? './whatsasena.db' : process.env.DATABASE_URL;
+DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBUG);
+module.exports = {
+    VERSION: 'beta',
+    USERNAME: process.env.USERNAME === undefined ? '' : process.env.USERNAME,
+    PASSWORD: process.env.PASSWORD === undefined ? '' : process.env.PASSWORD,
+    HEROKU: {
+        HEROKU: process.env.HEROKU === undefined ? false : convertToBool(process.env.HEROKU),
+        API_KEY: process.env.HEROKU_API_KEY === undefined ? '' : process.env.HEROKU_API_KEY,
+        APP_NAME: process.env.HEROKU_APP_NAME === undefined ? '' : process.env.HEROKU_APP_NAME
+    },
+    DATABASE_URL: DATABASE_URL,
+    SUDO: process.env.SUDO === undefined ? '917907162582,0' : process.env.SUDO,
+    DEBUG: DEBUG
+    };
