77e15fb94b6e80549b949aa95cf4981e504d523d
Sourav <87891508+souravkl11@users.noreply.github.com>
2022-04-12 22:00:53 +0530

    Fix warn

--- plugins/warn.js
+++ plugins/warn.js
@@ -2,12 +2,13 @@
  Warn: souravkl11/raganork md 
 You may not use this file except compliance with license!*/
 let {Module} = require('../main');
-let {WARN,USERNAME} = require('../config');
+let {WARN} = require('../config');
+let {user} = require('../lib/DecryptSession');
 let {setwarn,getwarn,deletewarn} = require('raganork-bot');
 Module({pattern: 'warn ?(.*)', fromMe: true, desc:'Warns user. Removes user after maximum number of warns'}, (async (m, mat) => { 
 if (!m.reply_message) return await m.client.sendMessage(m.jid, { text: '_Reply to any message_' },{ quoted: m.data })
 var par = m.reply_message.jid
-var me = USERNAME
+var me = user
 var chat = m.jid
 if (!chat.endsWith('@g.us')) return await m.client.sendMessage(m.jid, { text: '_Only works in groups_' },{ quoted: m.data })
 var warn = await setwarn(me,chat,par,WARN)
@@ -33,7 +34,7 @@ if (warn !== 0) {
 Module({pattern: 'reset warn', fromMe: true, desc:'Resets the warn count of the user'}, (async (m, mat) => { 
     if (!m.reply_message) return await m.sendMessage('_Reply to any message!_')
         var par = m.reply_message.jid
-    var me = USERNAME
+    var me = user
     var chat = m.jid
 if (!chat.endsWith('@g.us')) return await m.client.sendMessage(m.jid, { text: '_Only works in groups_' },{ quoted: m.data })
  await deletewarn(me,chat,par)
