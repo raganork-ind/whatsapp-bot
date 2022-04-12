054f1b0dccdb8204ec77d3c81f673097f1e4cedc
Sourav <87891508+souravkl11@users.noreply.github.com>
2022-04-12 17:25:15 +0530

    Added mode support for command alive

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,5 +1,7 @@
 const {Module} = require('../main')
-Module({pattern: 'alive', fromMe: true, desc: 'Is bot alive?'}, (async (message, match) => {
+const {MODE} = require('../config');
+let w = MODE=='public'?false:true
+Module({pattern: 'list', fromMe: w, desc: 'Is bot alive?'}, (async (message, match) => {
 const templateButtons = [
   {index: 1, urlButton: {displayText: 'Official website', url: 'https://bit.ly/Raganork'}},
   {index: 2, urlButton: {displayText: 'Raganork Github', phoneNumber: 'https://github.com/souravkl11/raganork'}},
@@ -16,8 +18,11 @@ const buttonMessage = {
 
 await message.client.sendMessage(message.jid, buttonMessage)
 }))
-Module({on: 'button', fromMe: true, desc: 'Is bot alive?'}, (async (message, match) => {
+Module({on: 'button', fromMe: w, desc: 'Is bot alive?'}, (async (message, match) => {
 	if (message.tembutton === 'mdcmd') await message.client.sendMessage(message.jid, { text: 'Commands will appear here!' },{ quoted: message.data })
 	if (message.tembutton === 'mdmenu') await message.client.sendMessage(message.jid, { text: 'Menu will appear here!' },{ quoted: message.data })
  
 }))
+Module({pattern: 'alive', fromMe: w, desc: 'Is bot alive?'}, (async (message, match) => {
+await message.client.sendMessage(message.jid, { text: 'I am alive' },{ quoted: message.data })
+ }))
