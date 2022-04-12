0298ab167122c4d7e4cb1a65c64972c26fb43b70
Sourav <87891508+souravkl11@users.noreply.github.com>
2022-04-12 21:32:12 +0530

    Added warn

---
+++ plugins/warn.js
@@ -0,0 +1,41 @@
+/* (c) coded by souravkl11 for
+ Warn: souravkl11/raganork md 
+You may not use this file except compliance with license!*/
+let {Module} = require('../main');
+let {WARN,USERNAME} = require('../config');
+let {setwarn,getwarn,deletewarn} = require('raganork-bot');
+e.addCommand({pattern: 'warn ?(.*)', fromMe: true, desc:'Warns user. Removes user after maximum number of warns'}, (async (m, mat) => { 
+if (!m.reply_message) return await m.client.sendMessage(m.jid, { text: '_Reply to any message_' },{ quoted: m.data })
+var par = m.reply_message.jid
+var me = USERNAME
+var chat = m.jid
+if (!chat.endsWith('@g.us')) return await m.client.sendMessage(m.jid, { text: '_Only works in groups_' },{ quoted: m.data })
+var warn = await setwarn(me,chat,par,WARN)
+var ms = 'Replied message';
+if (m.reply_message.audio) ms = 'Audio Message'
+if (m.reply_message.sticker) ms = 'Sticker Message'
+if (m.reply_message.text) ms = m.reply_message.text
+if (m.reply_message.video) ms = 'Video Message'
+if (m.reply_message.image) ms = 'Image Message'
+if (m.reply_message.data.quotedMessage.listMessage) ms = 'List message'
+var reason = mat[1] ? mat[1] : ms
+var msg = "```Warning ⚠️```"+ '\n' +
+"*User:* " +'@'+par.split('@')[0] + '\n' +
+"*Reason:* " + reason+ '\n' +
+"*Remaining:* "  + warn + '\n' 
+if (warn !== 0) {
+    return await m.client.sendMessage(m.jid, { text: msg ,mentions:[par]},{ quoted: m.data })
+} else {
+    await m.client.sendMessage(m.jid, { text: 'Warn limit ('+cnt+') of '+par+' exceeded. Removing participant ❌' ,mentions:[par]},{ quoted: m.data })
+    await m.client.groupParticipantsUpdate(m.jid, [par], "remove")
+ }
+}));
+e.addCommand({pattern: 'reset warn', fromMe: true, desc:'Resets the warn count of the user'}, (async (m, mat) => { 
+    if (!m.reply_message) return await m.sendMessage('_Reply to any message!_')
+        var par = m.reply_message.jid
+    var me = USERNAME
+    var chat = m.jid
+if (!chat.endsWith('@g.us')) return await m.client.sendMessage(m.jid, { text: '_Only works in groups_' },{ quoted: m.data })
+ await deletewarn(me,chat,par)
+    return await m.client.sendMessage(m.jid, { text: 'Successfully reset warn limits of @'+par ,mentions:[par]},{ quoted: m.data })
+}));
