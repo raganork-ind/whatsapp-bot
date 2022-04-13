e3744f5c0efb391d23988c5ee0bf1003457f7b8d
Sourav <87891508+souravkl11@users.noreply.github.com>
2022-04-13 13:14:32 +0530

    Fix

--- plugins/converters.js
+++ plugins/converters.js
@@ -1,5 +1,5 @@
 const {Module} = require('../main');
-const {downloadContentFromMessage,Mimetype} = require('@adiwajshing/baileys');
+const {downloadContentFromMessage } = require('@adiwajshing/baileys');
 const fs = require('fs');
 const ffmpeg = require('fluent-ffmpeg');
 const {execFile} = require('child_process');
@@ -7,12 +7,16 @@ const Config = require('../config');
 const {MODE} = require('../config');
 let w = MODE=='public'?false:true
 Module({pattern: 'sticker$', fromMe: w}, (async (message, match) => {    
-if (message.reply_message === false) return await message.client.sendMessage(message.jid, { text: '_Reply to a photo or short video_' })
- const stream = await downloadContentFromMessage(message.reply_message.data.quotedMessage[message.reply_message.image?'imageMessage':'videoMessage], message.reply_message.image?'image':'video)
- let buffer = Buffer.from([])
- for await(const chunk of stream) {
-  buffer = Buffer.concat([buffer, chunk])}
-  await fs.writeFileSync(message.reply_message.image?'./temp/sticker.jpeg':'./temp/sticker.mp4', buffer)
+
+        if (message.reply_message === false) return await message.client.sendMessage(message.jid, { text: '_Reply to a photo_' })
+        
+	const stream = await downloadContentFromMessage(message.reply_message.data.quotedMessage.imageMessage, 'image')
+        let buffer = Buffer.from([])
+        for await(const chunk of stream) {
+            buffer = Buffer.concat([buffer, chunk])
+        }
+        // save to file
+        await fs.writeFileSync('./temp/sticker.jpeg', buffer)
     
 
         if (message.reply_message.video === false && message.reply_message.image) {
@@ -26,7 +30,7 @@ if (message.reply_message === false) return await message.client.sendMessage(mes
         return;
         }
 
-        ffmpeg('./temp/sticker.mp4')
+        ffmpeg('')
             .outputOptions(["-y", "-vcodec libwebp", "-lossless 1", "-qscale 1", "-preset default", "-loop 0", "-an", "-vsync 0", "-s 600x600"])
             .videoFilters('scale=600:600:flags=lanczos:force_original_aspect_ratio=decrease,format=rgba,pad=600:600:(ow-iw)/2:(oh-ih)/2:color=#00000000,setsar=1')
             .save('./temp/st.webp')
@@ -35,17 +39,3 @@ if (message.reply_message === false) return await message.client.sendMessage(mes
             });
         return;
     }));
-Module({pattern: 'mp3$', fromMe: w}, (async (message, match) => {    
-if (message.reply_message === false) return await message.client.sendMessage(message.jid, { text: '_Reply to a photo_' })
- const stream = await downloadContentFromMessage(message.reply_message.data.quotedMessage[message.reply_message.audio?'audioMessage':'videoMessage'],message.reply_message.audio?'audio':'video')
- let buffer = Buffer.from([])
- for await(const chunk of stream) {
-  buffer = Buffer.concat([buffer, chunk])}
-  await fs.writeFileSync(message.reply_message.audio?'./temp/tomp3.mp3':'./temp/tomp3.mp4', buffer)
-  ffmpeg(message.reply_message.audio?'./temp/tomp3.mp3':'./temp/tomp3.mp4')
-            .save('./temp/resmp3.mp3')
-            .on('end', async () => {
-await message.client.sendMessage(message.jid, { audio: fs.readFileSync('./temp/resmp3.mp3') }, { quoted: message.data, mimetype: Mimetype.mp4audio })
-});
-}));
-     
