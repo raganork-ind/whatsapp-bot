ce55f4613a1b061fabca07071f2ac08145cf93f5
Sourav <87891508+souravkl11@users.noreply.github.com>
2022-04-13 17:21:04 +0530

    Fixed add,kick

--- plugins/group.js
+++ plugins/group.js
@@ -1,12 +1,16 @@
 const {Module} = require('../main')
-Module({pattern: 'kick', fromMe: true, desc: 'Removes participant'}, (async (message, match) => {
-await message.client.sendMessage(message.jid, { text: '@'+message.reply_message.jid.split('@')[0] +' kicked!', mentions: [message.reply_message.jid] })
-await message.client.groupParticipantsUpdate(message.jid, [message.reply_message.jid], "remove" /*replace this parameter with "remove", "demote" or "promote" */)
+Module({pattern: 'kick ?(.*)', fromMe: true, desc: 'Removes participant'}, (async (message, match) => {
+const user = message.mention[0] || message.reply_message.jid
+await message.client.sendMessage(message.jid, { text: '@'+user.split('@')[0] +' kicked!', mentions: [user] })
+await message.client.groupParticipantsUpdate(message.jid, [user], "remove" /*replace this parameter with "remove", "demote" or "promote" */)
 }))
-Module({pattern: 'add', fromMe: true, desc: 'Adds participants to groups'}, (async (message, match) => {
+Module({pattern: 'add ?(.*)', fromMe: true, desc: 'Adds participants to groups'}, (async (message, match) => {
+var init = match[1]
+var initt = init.split(" ").join("")
+var user = initt.replace(/+/g,'').replace(' ','').replace(' ','').replace(' ','').replace(' ','').replace(/(/g,'').replace(/)/g,'').replace(/-/g,'')
 var jids = [];
 var msg = '';
-numbers = match[1].split(',');
+numbers = user.split(',');
 numbers.map((number) => {
 msg += '@'+number+'\n'
 jids.push(number+'@s.whatsapp.net');});
@@ -28,10 +32,4 @@ await message.client.sendMessage(message.jid, { text: '_Group muted_' })
 Module({pattern: 'unmute', fromMe: true, desc: 'Allows all participants to msg in group'}, (async (message, match) => {
 await message.client.groupSettingUpdate(message.jid, 'not_announcement')
 await message.client.sendMessage(message.jid, { text: '_Group unmuted_' })
-}))
-Module({pattern: 'logm', fromMe: true}, (async (message, match) => {
-console.log(message)
-}))
-Module({pattern: 'logt', fromMe: true}, (async (message, match) => {
-console.log(match)
 }))
