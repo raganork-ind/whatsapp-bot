f89a5925b87ce9d61c0a79a66077e36d89850dcc
Sourav <87891508+souravkl11@users.noreply.github.com>
2022-04-15 05:05:37 +0530

    Fix

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const { Sequelize } = require('sequelize');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
