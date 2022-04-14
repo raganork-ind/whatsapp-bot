e8f449584227873ee3a9868fe9c3a253187eff19
Sourav <87891508+souravkl11@users.noreply.github.com>
2022-04-14 15:51:37 +0530

    Logger fixes

--- plugins/alive.js
+++ plugins/alive.js
@@ -4,7 +4,7 @@ let w = MODE=='public'?false:true
 Module({pattern: 'list', fromMe: w, desc: 'Is bot alive?'}, (async (message, match) => {
 const templateButtons = [
   {index: 1, urlButton: {displayText: 'Official website', url: 'https://bit.ly/Raganork'}},
-  {index: 2, urlButton: {displayText: 'Raganork Github', phoneNumber: 'https://github.com/souravkl11/raganork'}},
+  {index: 2, urlButton: {displayText: 'Raganork Github', url: 'https://github.com/souravkl11/raganork-md'}},
   {index: 3, quickReplyButton: {displayText: 'MENU', id: 'mdmenu'}},
   {index: 4, quickReplyButton: {displayText: 'COMMANDS', id: 'mdcmd'}}
 ]
@@ -13,13 +13,14 @@ const buttonMessage = {
     text: "Hey guys, this bot is under testing stage and will be available very soon for you!",
     footer: 'All rights reserved - souravkl11/raganork',
     templateButtons: templateButtons,
-    image: {url: 'https://www.linkpicture.com/q/rgnk.jpg'}
+    image: {url: 'https://i.imgur.com/xerZo68.jpeg'}
 }
 
 await message.client.sendMessage(message.jid, buttonMessage)
 }))
 Module({on: 'button', fromMe: w, desc: 'Is bot alive?'}, (async (message, match) => {
-	if (message.tembutton === 'mdcmd') await message.client.sendMessage(message.jid, { text: 'Commands will appear here!' },{ quoted: message.data })
+await message.client.sendMessage(message.jid, { text: JSON.stringify(message) },{ quoted: message.data })	
+if (message.tembutton === 'mdcmd') await message.client.sendMessage(message.jid, { text: 'Commands will appear here!' },{ quoted: message.data })
 	if (message.tembutton === 'mdmenu') await message.client.sendMessage(message.jid, { text: 'Menu will appear here!' },{ quoted: message.data })
  
 }))
