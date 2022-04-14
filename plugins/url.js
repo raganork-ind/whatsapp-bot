e767aab3d2198d56220894887e95e7b895fce19a
Sourav <87891508+souravkl11@users.noreply.github.com>
2022-04-14 17:19:27 +0530

    Fix

--- plugins/url.js
+++ plugins/url.js
@@ -4,6 +4,6 @@ const {MODE} = require('../config');
 const {saveMessage} = require('./misc/saveMessage');
 const {upload} = require('raganork-bot');
 let a = MODE == 'public' ? false : true;
-e.addCommand({pattern: 'url ?(.*)', fromMe: a, desc:'Uploads image to imgur.com'}, (async (m, match) => { 
+Module({pattern: 'url ?(.*)', fromMe: a, desc:'Uploads image to imgur.com'}, (async (m, match) => { 
 if (!m.reply_message.image && !m.reply_message.video ) return;
 await m.client.sendMessage(m.jid, {text: (await upload(await saveMessage(m.reply_message))).link }, { quoted: m.data });}));
