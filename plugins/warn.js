62e4cacf179481d3db98c7062f0ad0e789899d6b
Sourav <87891508+souravkl11@users.noreply.github.com>
2022-04-15 07:56:05 +0530

    Update warn.js

--- plugins/warn.js
+++ plugins/warn.js
@@ -7,8 +7,9 @@ let {setwarn,getwarn,deletewarn} = require('raganork-bot');
 Module({pattern: 'warn ?(.*)', fromMe: true, desc:'Warns user. Removes user after maximum number of warns'}, (async (m, mat) => { 
 if (!m.reply_message) return await m.client.sendMessage(m.jid, { text: '_Reply to any message_' },{ quoted: m.data })
 var par = m.reply_message.jid
-var me = m.client.user.id.split('@')[0].split(':')[0]
-if (par.split('@')[0] === me) return await m.client.sendMessage(m.jid, { text: '_I won't warn me 😌_' },{ quoted: m.data })
+var me = m.client.user.id.split('@')[0].split(':')[0];
+var rj = par.split('@')[0];
+if (rj == me) return await m.client.sendMessage(m.jid, { text: '_I won't warn me 😌_' },{ quoted: m.data })
 var chat = m.jid
 if (!chat.endsWith('@g.us')) return await m.client.sendMessage(m.jid, { text: '_Only works in groups_' },{ quoted: m.data })
 var warn = await setwarn(me,chat,par, parseInt(WARN))
