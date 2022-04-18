8bd830ff8a73b70dec87b8f53d57c9db8197c99a
Sourav <87891508+souravkl11@users.noreply.github.com>
2022-04-18 18:26:23 +0530

    Fix lang loader

--- plugins/misc/lang.js
+++ plugins/misc/lang.js
@@ -1,5 +1,5 @@
 const {LANGUAGE} = require('../../config');
 const {existsSync,readFileSync} = require('fs');
-var json = fs.existsSync('./lang/' + LANGUAGE + '.json') ? JSON.parse(fs.readFileSync('./language/' + LANGUAGE + '.json')) : JSON.parse(fs.readFileSync('./language/english.json'));
+var json = fs.existsSync('./lang/' + LANGUAGE + '.json') ? JSON.parse(fs.readFileSync('./lang/' + LANGUAGE + '.json')) : JSON.parse(fs.readFileSync('./lang/english.json'));
 function getString(file) { return json['STRINGS'][file]; }
 module.exports = {language: json, getString: getString }
