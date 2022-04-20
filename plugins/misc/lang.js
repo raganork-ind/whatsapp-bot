d6722a15e716a628f55530d62c586a6a69d8cff4
Sourav <87891508+souravkl11@users.noreply.github.com>
2022-04-20 13:13:06 +0530

    fix

--- plugins/misc/lang.js
+++ plugins/misc/lang.js
@@ -1,5 +1,5 @@
 const {LANGUAGE} = require('../../config');
 const {existsSync,readFileSync} = require('fs');
-var json = existsSync('./lang/' + LANGUAGE + '.json') ? JSON.parse(readFileSync('./lang/' + LANGUAGE + '.json')) : JSON.parse(readFileSync('./lang/english.json'));
+var json = existsSync('/skl/Raganork/plugins/misc/lang/' + LANGUAGE + '.json') ? JSON.parse(readFileSync('/skl/Raganork/plugins/misc/lang/' + LANGUAGE + '.json')) : JSON.parse(readFileSync('/skl/Raganork/plugins/misc/lang/english.json'));
 function getString(file) { return '```'+json['STRINGS'][file]+'```'; }
 module.exports = {language: json, getString: getString }
