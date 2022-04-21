d01dd90b9e6afae737fcd8aff6bb4166913e50ef
Sourav <87891508+souravkl11@users.noreply.github.com>
2022-04-21 18:45:55 +0530

    Added story downloader

--- plugins/instagram.js
+++ plugins/instagram.js
@@ -43,25 +43,22 @@ Module({ pattern: 'ig ?(.*)', fromMe: sourav, desc:'Gets account info from insta
     var buffer = await skbuffer(res.hd_profile_pic_url_info.url)
     await msg.client.sendMessage(msg.jid, { image: buffer, caption: '_*Name:*_ ' + `${res.fullname}` + '\n _*Bio:*_ ' + `${res.biography}`+ '\n _*Private account:*_ ' + `${res.is_private} ` + '\n _*Followers:*_ ' + `${res.followers}` + '\n _*Following:*_ ' + `${res.following}` + '\n _*Posts:*_ ' + `${res.post_count}` + '\n _*Verified:*_ ' + `${res.is_verified} ` + '\n _*IGTV videos:*_ ' + `${res.total_igtv_videos}`}, {quoted: msg.data});
     }));
-/*
-skl.addCommand({ pattern: 'story ?(.*)', fromMe: sourav, desc:'Downloads full/single story from instagram',usage:'.story username or link'}, (async (msg, query) => {
-if (query[1] === '') return await msg.client.sendMessage(msg.jid, need_acc_s, MessageType.text, {quoted: msg.data});
+Module({ pattern: 'story ?(.*)', fromMe: sourav, desc:'Downloads full/single story from instagram',usage:'.story username or link'}, (async (msg, query) => {
+if (query[1] === '') return await msg.sendReply(need_acc_s);
 var user = query[1];
 var res = await getStory(user,v)
-if (res === "false") return await msg.client.sendMessage(msg.jid, "_Story not found!_", MessageType.text, {quoted: msg.data})
-if (res.error) return await msg.client.sendMessage(msg.jid, res.error.replace('status','story'), MessageType.text, {quoted: msg.data})
+if (res === "false") return await msg.sendReply("_Story not found!_")
+if (res.error) return await msg.sendReply("Status: 403 (forbidden)")
 var url = ''
-await msg.sendMessage('```Downloading '+res.result.stories.length+' stories of '+res.result.username+'```');
+await msg.sendMessage('```Downloading '+res.result.stories.length+' stories of '+ res.result.username || query[1] +'```');
 res.result.stories.map((result) => {
 url += result.url + ','});
 var que = url !== false ? url.split(',') : [];
 for (var i = 0; i < (que.length < res.result.stories.length ? que.length : res.result.stories.length); i++) {
 var get = got(que[i], {https: {rejectUnauthorized: false}});
-var type = que[i].includes('mp4') ? MessageType.video : MessageType.image
-var mime = que[i].includes('mp4') ? Mimetype.mp4 : Mimetype.jpg
+var type = que[i].includes('mp4') ? 'video' : 'image'
 var stream = get.buffer();
 stream.then(async (video) => {
-await msg.client.sendMessage(msg.jid, video, type, { mimetype: mime,quoted: msg.data});
+await msg.sendReply(video,type);
 })};
 }));
-*/
