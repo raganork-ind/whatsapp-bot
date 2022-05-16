62312f05c24cec05c302251fc79d1a4c15e54eab
Sourav <87891508+souravkl11@users.noreply.github.com>
2022-05-16 17:11:34 +0530

    fix

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -212,27 +212,26 @@ Module({
 }, async (message, match) => {
     if (match[1] === '') return await message.sendReply('```Give me a movie name 👀.```');
 	var {data} = await axios(`http://www.omdbapi.com/?apikey=742b2d09&t=${match[1]}&plot=full`);
-	var json = JSON.parse(data);
-	if (json.Response != 'True') return await message.sendReply(json.Error);
+	if (data.Response != 'True') return await message.sendReply(data.Error);
 	let msg = '```';
-	msg += 'Title      : ' + json.Title + '\n\n';
-	msg += 'Year       : ' + json.Year + '\n\n';
-	msg += 'Rated      : ' + json.Rated + '\n\n';
-	msg += 'Released   : ' + json.Released + '\n\n';
-	msg += 'Runtime    : ' + json.Runtime + '\n\n';
-	msg += 'Genre      : ' + json.Genre + '\n\n';
-	msg += 'Director   : ' + json.Director + '\n\n';
-	msg += 'Writer     : ' + json.Writer + '\n\n';
-	msg += 'Actors     : ' + json.Actors + '\n\n';
-	msg += 'Plot       : ' + json.Plot + '\n\n';
-	msg += 'Language   : ' + json.Language + '\n\n';
-	msg += 'Country    : ' + json.Country + '\n\n';
-	msg += 'Awards     : ' + json.Awards + '\n\n';
-	msg += 'BoxOffice  : ' + json.BoxOffice + '\n\n';
-	msg += 'Production : ' + json.Production + '\n\n';
-	msg += 'imdbRating : ' + json.imdbRating + '\n\n';
-	msg += 'imdbVotes  : ' + json.imdbVotes + '```';
-    var posterApi = (await axios(`https://api.themoviedb.org/3/search/movie?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&query=${json.Title}`)).data
-    var poster = posterApi.total_results !== 0 ? "https://image.tmdb.org/t/p/w500/"+posterApi.results[0].poster_path : json.Poster
+	msg += 'Title      : ' + data.Title + '\n\n';
+	msg += 'Year       : ' + data.Year + '\n\n';
+	msg += 'Rated      : ' + data.Rated + '\n\n';
+	msg += 'Released   : ' + data.Released + '\n\n';
+	msg += 'Runtime    : ' + data.Runtime + '\n\n';
+	msg += 'Genre      : ' + data.Genre + '\n\n';
+	msg += 'Director   : ' + data.Director + '\n\n';
+	msg += 'Writer     : ' + data.Writer + '\n\n';
+	msg += 'Actors     : ' + data.Actors + '\n\n';
+	msg += 'Plot       : ' + data.Plot + '\n\n';
+	msg += 'Language   : ' + data.Language + '\n\n';
+	msg += 'Country    : ' + data.Country + '\n\n';
+	msg += 'Awards     : ' + data.Awards + '\n\n';
+	msg += 'BoxOffice  : ' + data.BoxOffice + '\n\n';
+	msg += 'Production : ' + data.Production + '\n\n';
+	msg += 'imdbRating : ' + data.imdbRating + '\n\n';
+	msg += 'imdbVotes  : ' + data.imdbVotes + '```';
+    var posterApi = (await axios(`https://api.themoviedb.org/3/search/movie?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&query=${data.Title}`)).data
+    var poster = posterApi.total_results !== 0 ? "https://image.tmdb.org/t/p/w500/"+posterApi.results[0].poster_path : data.Poster
     return await message.client.sendMessage(message.jid,{image: {url: poster}, caption:msg},{quoted: message.data})
 });
