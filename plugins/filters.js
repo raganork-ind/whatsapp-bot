bcd3d70c7609abf3b76c1ac2115ac43890b5fa12
Sourav <87891508+souravkl11@users.noreply.github.com>
2022-05-31 10:53:52 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -15,7 +15,7 @@ Module({
 }, (async (message, match) => {
     match = match[1].match(/[\'\"\“](.*?)[\'\"\“]/gsm);
     if (message.reply_message.text) {
-        await FilterDb.setFilter(message.jid, encodeURIComponent(match[0].replace(/['"“]+/g, '')), message.reply_message.text, match[0][0] === "'" ? true : false);
+        await FilterDb.setFilter(message.jid, match[0].replace(/['"“]+/g, ''), message.reply_message.text, match[0][0] === "'" ? true : false);
         await message.client.sendMessage(message.jid, {
             text: "_Set_ " + match[0].replace(/['"]+/g, '') + " _to filter ✅_"
         });
@@ -31,7 +31,7 @@ Module({
             var mesaj = "_Your filters in this chat:_" + '\n';
             filtreler.map((filter) => mesaj += '```' + filter.dataValues.pattern + '```\n');
             await message.client.sendMessage(message.jid, {
-                text: decodeURIComponent(mesaj)
+                text: mesaj
             });
         }
     } else {
@@ -104,7 +104,7 @@ Module({
     filtreler.map(
         async (filter) => {
             pattern = new RegExp(filter.dataValues.regex ? filter.dataValues.pattern : ('\\b(' + filter.dataValues.pattern + ')\\b'), 'gm');
-            if (pattern.test(encodeURIComponent(Text))) {
+            if (pattern.test(Text)) {
                 await message.client.sendMessage(message.jid, {
                     text: filter.dataValues.text
                 }, {
