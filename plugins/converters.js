4e919373a201cf247dbd5a1e9a2b9fbe0bf5ca8a
Sourav <87891508+souravkl11@users.noreply.github.com>
2022-05-31 17:21:25 +0530

    Fix stickers test

--- plugins/converters.js
+++ plugins/converters.js
@@ -41,12 +41,11 @@ Module({
         android: "https://github.com/souravkl11/Raganork-md/",
         ios: "https://github.com/souravkl11/Raganork-md/"
     }
-    if (message.reply_message.video === false && message.reply_message.image) {
-        await message.sendReply(fs.readFileSync(await addExif(await sticker(savedFile),exif)), 'sticker')
-        return;
+    if (message.reply_message.image === true) {
+        return await message.sendReply(fs.readFileSync(await addExif(await sticker(savedFile),exif)), 'sticker')
+     } else {
+        return await message.sendReply(fs.readFileSync(await addExif(await sticker(savedFile),exif)), 'sticker')
     }
-        await message.sendReply(fs.readFileSync(await addExif(await sticker(savedFile),exif)), 'sticker')
-        return;
 }));
 Module({
     pattern: 'mp3$',
