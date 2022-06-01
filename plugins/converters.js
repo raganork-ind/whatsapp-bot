1911dba2e7155564e45337431dfc037854aeb93e
Sourav <87891508+souravkl11@users.noreply.github.com>
2022-06-01 12:54:04 +0530

    Fix video => Sticker size issue

--- plugins/converters.js
+++ plugins/converters.js
@@ -44,7 +44,7 @@ Module({
     if (message.reply_message.image === true) {
         return await message.sendReply(fs.readFileSync(await addExif(await sticker(savedFile),exif)), 'sticker')
      } else {
-        return await message.sendReply(fs.readFileSync(await sticker(savedFile)), 'sticker')
+        return await message.sendReply(fs.readFileSync(await addExif(await sticker(savedFile,'video'),exif)), 'sticker')
     }
 }));
 Module({
